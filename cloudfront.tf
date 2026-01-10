7c5466457acd8f6cbdafac84fbe0876d0c5c8b76
David Kong <dkong535@gmail.com>
2026-01-09 23:53:46 -0600

    Terraform fmt

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,93 +1,93 @@
 data "aws_route53_zone" "main" {
-    name = var.domain_name
-    private_zone = false
+  name         = var.domain_name
+  private_zone = false
 }
 
 resource "aws_acm_certificate" "cert" {
-    provider = aws.us_east_1
-    domain_name = var.domain_name
-    validation_method = "DNS"
+  provider          = aws.us_east_1
+  domain_name       = var.domain_name
+  validation_method = "DNS"
 
-    lifecycle {
-        create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_route53_record" "cert_validation" {
-    for_each = {
-        for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => dvo
-    }
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => dvo
+  }
 
-    allow_overwrite = true
-    name = each.value.resource_record_name
-    records = [each.value.resource_record_value]
-    ttl = 60
-    type = each.value.resource_record_type
-    zone_id = data.aws_route53_zone.main.zone_id
+  allow_overwrite = true
+  name            = each.value.resource_record_name
+  records         = [each.value.resource_record_value]
+  ttl             = 60
+  type            = each.value.resource_record_type
+  zone_id         = data.aws_route53_zone.main.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-    provider = aws.us_east_1
-    certificate_arn = aws_acm_certificate.cert.arn
-    validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
-    enabled = true
-    default_root_object = "index.html"
-    aliases = [var.domain_name]
+  enabled             = true
+  default_root_object = "index.html"
+  aliases             = [var.domain_name]
 
-    origin {
-        domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
-        origin_id = "S3-${var.domain_name}"
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
+    origin_id   = "S3-${var.domain_name}"
 
-        custom_origin_config {
-            http_port = 80
-            https_port = 443
-            origin_protocol_policy = "http-only"
-            origin_ssl_protocols = ["TLSv1.2"]
-        }
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${var.domain_name}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
     }
 
-    default_cache_behavior {
-        allowed_methods = ["GET", "HEAD"]
-        cached_methods = ["GET", "HEAD"]
-        target_origin_id = "S3-${var.domain_name}"
+    viewer_protocol_policy = "redirect-to-https" # force HTTPS
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-        forwarded_values {
-            query_string = false
-            cookies {
-                forward = "none"
-            }
-        }
-
-        viewer_protocol_policy = "redirect-to-https" # force HTTPS
-        min_ttl = 0
-        default_ttl = 3600
-        max_ttl = 86400
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
 
-    restrictions {
-        geo_restriction {
-            restriction_type = "none"
-        }
-    }
-
-    viewer_certificate {
-        acm_certificate_arn = aws_acm_certificate.cert.arn
-        ssl_support_method = "sni-only"
-        minimum_protocol_version = "TLSv1.2_2021"
-    }
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 }
 
 resource "aws_route53_record" "www" {
-    zone_id = data.aws_route53_zone.main.zone_id
-    name = var.domain_name
-    type = "A"
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
 
-    alias {
-        name = aws_cloudfront_distribution.s3_distribution.domain_name
-        zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-        evaluate_target_health = false
-    }
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
